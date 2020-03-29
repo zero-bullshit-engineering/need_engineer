b9d57371583b8139ac96f8c7b71ed4f5c117b96e
Tim Buchwaldt <tim.buchwaldt@grandcentrix.net>
2020-03-29 15:54:55 +0200

    Fix slipup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -55,7 +55,7 @@ RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     # This option will default to `true` in RSpec 4. It makes the `description`
     # and `failure_message` of custom matchers include text for helper methods
-    # defined using `chpain`, e.g.:
+    # defined using `chain`, e.g.:
     #     be_bigger_than(2).and_smaller_than(4).description
     #     # => "be bigger than 2 and smaller than 4"
     # ...rather than:
